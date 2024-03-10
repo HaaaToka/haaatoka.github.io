7ced6df93fd6661b4ff36941bfb92d37aa12ef2d
HaaaToka <okanalan97.14@gmail.com>
2024-03-10 22:35:26 +0300

    add snacks and colgroup to table

--- js/magic.js
+++ js/magic.js
@@ -97,6 +97,19 @@ function createRWLTable(year) {
         rwltablediv.innerText = "";
 
         var rwltable = document.createElement("table");
+
+        var tablecolgroup = document.createElement("colgroup");
+        var col1 = document.createElement("col");
+        col1.setAttribute("width", "8%");
+        var col2 = document.createElement("col");
+        col2.setAttribute("width", "40%");
+        var col3 = document.createElement("col");
+        col3.setAttribute("width", "60%");
+        tablecolgroup.appendChild(col1);
+        tablecolgroup.appendChild(col2);
+        tablecolgroup.appendChild(col3);
+        rwltable.appendChild(tablecolgroup);
+
         var thead = document.createElement("thead");
         var headtr = document.createElement("tr");
     
@@ -125,6 +138,72 @@ function createRWLTable(year) {
           link.appendChild(linka);
     
           bodytr.appendChild(date);
+          bodytr.appendChild(title);
+          bodytr.appendChild(link);
+    
+          tbody.appendChild(bodytr);
+        });
+    
+        rwltable.appendChild(tbody);
+    
+        rwltablediv.appendChild(rwltable);
+      }else{
+        var rwltablediv = document.getElementById(elementid);
+        rwltablediv.innerText = "Calm Down!";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  });
+
+}
+
+function createSnackTable() {
+
+  elementid = "rwl-table-div";
+  readTextFile("/posts/rwl/rwl_list.json", function(rwlList){
+    try {
+      if( Object.keys(rwlList).includes("snacks") ){
+        var rwltablediv = document.getElementById(elementid);
+        rwltablediv.innerText = "";
+
+        var rwltable = document.createElement("table");
+
+        var tablecolgroup = document.createElement("colgroup");
+        var col1 = document.createElement("col");
+        col1.setAttribute("width", "40%");
+        var col2 = document.createElement("col");
+        col2.setAttribute("width", "60%");
+        tablecolgroup.appendChild(col1);
+        tablecolgroup.appendChild(col2);
+        rwltable.appendChild(tablecolgroup);
+
+        var thead = document.createElement("thead");
+        var headtr = document.createElement("tr");
+    
+        ["Title", "Link"].forEach(column => {
+          var headth = document.createElement("th");
+          headth.innerText = column;
+          headtr.appendChild(headth);
+        })
+        thead.appendChild(headtr);
+        rwltable.appendChild(thead);
+    
+        var tbody = document.createElement("tbody");
+    
+        Object.entries(rwlList["snacks"]).reverse().forEach(entry=>{
+          var bodytr = document.createElement("tr");
+    
+          var title = document.createElement("td");
+          title.innerText = entry[1][0];
+          var link = document.createElement("td");
+          var linka = document.createElement("a");
+          linka.innerText = entry[1][1];
+          linka.setAttribute("href",entry[1][1]);
+    
+          link.appendChild(linka);
+
           bodytr.appendChild(title);
           bodytr.appendChild(link);
     
